efb4f1402e0d74241107ddc90ee227ec90cf80d9
David Young <bosshog@passivelogic.com>
2023-06-20 17:49:44 -0400

    Initial code for generator

---
+++ src/generate.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+# This program may be used to create the data set for our Shakespearian Insult Generator.
+
+import sys
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    try:
+        main()
+        sys.exit(0)
+    except KeyboardInterrupt:
+        sys.exit(1)
