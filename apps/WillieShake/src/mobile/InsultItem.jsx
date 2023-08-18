c435f0d6f9378f7d16d4e5c89bf7b1817602cfd5
David Young <youngde811@pobox.com>
2023-08-18 13:54:37 -0400

    Add memoized list component

---
+++ apps/WillieShake/src/mobile/InsultItem.jsx
@@ -0,0 +1,47 @@
+// -*- mode: rjsx; eval: (auto-fill-mode 1); -*-
+
+// This component offers a memoized implementation of our FlatList insult items.
+
+// MIT License
+
+// Copyright (c) 2023 David Young
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+// documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+// Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import React, { memo, useRef, useState } from 'react';
+
+import { Animated, Button, Clipboard, FlatList, Text, TouchableOpacity, View } from 'react-native';
+import { Divider } from "@rneui/themed";
+import { Surface } from 'react-native-paper';
+
+import PressableOpacity from './PressableOpacity';
+import TouchableIcon from './TouchableIcon';
+
+import styles from '../styles/styles.js';
+
+function InsultItem({ item, insultSelect, storeFavorite, selectedInsult, showEasterEgg }) {
+    return (
+        <View style={ styles.insultItemContainer }>
+          <PressableOpacity style={ null } onPress={ () => insultSelect(item) }
+                            onLongPress={ () => storeFavorite(item) } delayLongPress={ 500 }>
+            <Text style={ item.insult == selectedInsult ? styles.insultSelectedText : styles.insultText }>
+              { item.insult }
+            </Text>
+          </PressableOpacity>
+          <TouchableIcon visible={ item.url.length > 0 } onPress={ () => showEasterEgg(item) }/>
+        </View>
+    );
+};
+
+export default memo(InsultItem);
