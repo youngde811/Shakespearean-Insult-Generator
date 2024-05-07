a2748add23e80875a7ea404a6f792963735c23f5
David Young <sporty-iron883@protonmail.com>
2024-05-07 15:27:31 -0400

    SettingsPage component

---
+++ apps/WillieShake/src/mobile/SettingsPage.jsx
@@ -0,0 +1,50 @@
+// -*- mode: rjsx; eval: (auto-fill-mode 1); -*-
+
+// This component is used to render a page that manages the application settings, independently of
+// the device operating system being used.
+
+// MIT License
+
+// Copyright (c) 2023 David Young
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+// documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+// Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import React, { useEffect, useRef, useState } from 'react';
+
+import { StatusBar } from 'expo-status-bar';
+import { ActivityIndicator, Button, ImageBackground, Text, View } from 'react-native';
+import { Surface } from 'react-native-paper';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import styles from '../styles/styles.js';
+
+import PressableOpacity from './PressableOpacity';
+import FloatingPressable from './FloatingPressable';
+
+export default function SettingsPage({ appConfig, background, setDismiss }) {
+    return (
+        <ImageBackground source={ background } resizeMode='cover' style={ styles.backgroundImage }>
+          <SafeAreaView edges={['bottom', 'left', 'right']} style={ styles.fjbTopView }>
+            <StatusBar style='auto'/>
+            <View style={ styles.codeWordsHeaderView }>
+              <View style={ styles.codeWordsFooter }>
+                <PressableOpacity style={ styles.codeWordsButtons } title={ 'Dismiss' } onPress={ setDismiss }>
+                  <Text style={ styles.codeWordsButtonText }>Dismiss</Text>
+                </PressableOpacity>
+              </View>
+            </View>
+          </SafeAreaView>
+        </ImageBackground>
+    );
+}
