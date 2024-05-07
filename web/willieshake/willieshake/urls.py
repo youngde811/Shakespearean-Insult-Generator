8d72e72ee07c3b82d81b16894bbc4d62e3789535
David Young <sporty-iron883@protonmail.com>
2024-05-06 20:02:42 -0400

    Do not need rest_framework

--- web/willieshake/willieshake/urls.py
+++ web/willieshake/willieshake/urls.py
@@ -37,6 +37,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("codewords/", include("codewords.urls")),
-    path("codewords/", include('rest_framework.urls')),
     path("admin/", admin.site.urls),
 ]
