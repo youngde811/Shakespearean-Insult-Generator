3f54006533a386112480287245ddda12a2191542
David Young <david.young@liveoak.bank>
2024-07-02 13:21:06 -0400

    Command-line parsing works

--- src/genrust/src/main.rs
+++ src/genrust/src/main.rs
@@ -8,20 +8,26 @@ use tikv_jemallocator::Jemalloc;
 #[global_allocator]
 static GLOBAL: Jemalloc = Jemalloc;
 
+/// A Shakespearian insult generator
 #[derive(Parser, Debug)]
 struct Args {
+    /// the number of insults to generate
     #[clap(short, long, default_value_t = 0)]
     count: usize,
 
+    /// the location of the phrases source file
     #[clap(short, long, default_value = "data/phrases")]
     phrases: String,
 
+    /// the destination file, if any, for the generated insults
     #[clap(short, long, default_value = "")]
     genfile: String,
 
+    /// the format for the generated insults
     #[clap(short, long, default_value = "json")]
     format: String,
 
+    /// the location of the URLs source file
     #[clap(short, long, default_value = "data/urls.json")]
     urls: String
 }
