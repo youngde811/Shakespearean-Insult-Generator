d71cd0bdb3d885c530a14a2c0feb089ead15d587
David Young <david.young@liveoak.bank>
2024-07-10 13:01:36 -0400

    Why is Rust such a pain?

--- src/genrust/src/main.rs
+++ src/genrust/src/main.rs
@@ -7,8 +7,7 @@ use std::io::{BufRead, BufReader};
 
 use regex::Regex;
 
-#[macro_use]
-extern crate json;
+use serde_json::json;
 
 use tikv_jemallocator::Jemalloc;
 
@@ -20,7 +19,7 @@ static GLOBAL: Jemalloc = Jemalloc;
 #[clap(version)]
 struct Args {
     /// the number of insults to generate
-    #[clap(short = 'c', long, default_value_t = 0)]
+    #[clap(short = 'c', long, default_value_t = 1)]
     count: i32,
 
     /// the location of the phrases source file
@@ -67,20 +66,29 @@ fn readlines(path: &str) -> Vec<Vec<String>> {
     results
 }
 
-fn insult_me(phrases: &json::JsonValue, ninsults: i32) {
-    for _i in 0..ninsults {
-        for keywords in phrases.members() {
-            println!("Thou {} {} {}!", keywords[0], keywords[1], keywords[2]);
+fn insult_me(phrases: &serde_json::Value, ninsults: i32) {
+    let mut i = 0;
+
+    while i < ninsults {
+        if let serde_json::Value::Array(tuples) = &phrases["phrases"] {
+            for entry in tuples {
+                let c1 = &entry[0].to_string().trim_matches(|c| c == '\"' || c == '\'').to_string();
+                let c2 = &entry[1].to_string().trim_matches(|c| c == '\"' || c == '\'').to_string();
+                let c3 = &entry[2].to_string().trim_matches(|c| c == '\"' || c == '\'').to_string();
+
+                println!("Thou {} {} {}!", c1, c2, c3);
+            }
+
+            i += 1;
         }
     }
-
 }
 
-fn load_phrases(phrases: String) -> json::JsonValue {
+fn load_phrases(phrases: String) -> serde_json::Value {
     let data = readlines(&phrases);
-    let phrases = object!{
+    let phrases = json!({
         "phrases": data
-    };
+    });
 
     phrases
 }
@@ -89,8 +97,6 @@ fn main() {
     let args = Args::parse();
 
     let phrases = load_phrases(args.phrases);
-    
-    println!("{:#}", phrases);
 
     let ninsults: i32 = if args.count > 0 {
         i32::from(args.count)
